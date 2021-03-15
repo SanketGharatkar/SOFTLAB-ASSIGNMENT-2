c3160c4f8f1e526ab1a70bdcafa48f478f85944c
Sanket <sanketgharatkar123@gmail.com>
2021-03-15 07:12:53 +0530

    added for addition

---
+++ add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("\nEnter two no: ");
+    scanf("%d %d", &a, &b);
+
+    sum = a + b;
+
+    printf("Sum : %d\n", sum);
+    printf("Shanky");
+
+    return (0);
+}
